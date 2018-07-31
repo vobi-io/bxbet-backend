20bc3a2e2c39769e7b67c2ea38cad61068e52fcc
Giga <giga.chkhikvadze@gmail.com>
2018-07-31 17:58:14 +0400

    increase free amount

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
 var Betting = artifacts.require("./Betting.sol")
 
 module.exports = function(deployer) {
-  var initialSupply = 175000000
+  var initialSupply = 17500000000
   var tokenName = 'BXBet'
   var tokenSymbol = 'BX'
   deployer.deploy(Betting, initialSupply, tokenName, tokenSymbol);
